1a62773982a0352e41126e0ddeed2c4bdb915e80
Aya Nady <ayanady22193@gmail.com>
2023-10-18 18:03:08 +0300

    sliding animation done

---
+++ lib/features/Splash/presentatin/widgets/slinding_animation.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SlidingAnimation extends StatelessWidget {
+  const SlidingAnimation({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, _) => SlideTransition(
+        position: slidingAnimation,
+        child: const Text(
+          "Read Free Books",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
