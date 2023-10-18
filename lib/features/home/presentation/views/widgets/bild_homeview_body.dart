983c43253f5e127b76302f29317c55733109761f
Aya Nady <ayanady22193@gmail.com>
2023-10-18 18:37:57 +0300

    custom app bar for home screen done

---
+++ lib/features/home/presentation/views/widgets/bild_homeview_body.dart
@@ -0,0 +1,42 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BuildHomeViewBody extends StatelessWidget {
+  const BuildHomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 48),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 18,
+          ),
+          Spacer(),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 24,
+              ))
+        ],
+      ),
+    );
+  }
+}
